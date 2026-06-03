c20adb3eb78591bc811303d2f196a9f55f3c13d1
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2026-06-03 19:44:59 +0300

    fix

--- jardump.sh
+++ jardump.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 ZB_JAR=`realpath ~/"projects/zomboid/versions/unstable/osx/Project Zomboid.app/Contents/Java/projectzomboid.jar"`
-CP="java/build/jdk25/libs/ZombieBuddy.jar:${ZB_JAR}"
+CP="$(dirname $0)/java/build/jdk25/libs/ZombieBuddy.jar:${ZB_JAR}"
 
 java -cp "$CP" me.zed_0xff.zombie_buddy.jardump.Main "$@"
